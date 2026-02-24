3d8dc51d9cdd8ad9ba5eb2ad248ae4a2ccf9ce14
VitoriaMassulo <vitoriamassulo@gmail.com>
2026-02-24 15:54:25 +0200

    1234

--- 22.02_homework.py
+++ 22.02_homework.py
@@ -56,5 +56,66 @@ def basic_math_list_2(a, b):
     sum_result.append(div_result)
     return sum_result
 
-result = basic_math_list_2(8, 2)
+result_1 = basic_math_list_2(8, 2)
 print(result)
+print('-'*25)
+
+#✨✨ Question 3 – Bonus challenge: Mini math game -- Experts only! ✨✨
+
+#Implement a small game
+#The computer should:
+
+#Pick a random number between 1 and 10
+#Pick a random operation from: +, -, *, %
+#Pick another random number between 1 and 10
+#Print the equation and ask the user for the result
+#Rules:
+
+#if the user is correct, print Correct!
+#Otherwise print Wrong.. the answer was ___
+#Your task
+
+#Your job is to complete the missing functions:
+
+#####get_random_between_1_10()
+#####get_random_operation()
+#####alc_result(num1, oper, num2)
+
+
+#resolution
+
+
+import random
+
+def get_random_between_1_10() -> int:
+    num = random.randint(1, 10)
+    return num
+
+def get_random_operation() -> str:
+    operator = random.choice(['+', '-', '*', '%'])
+    return operator
+
+def calc_result(num1, oper, num2):
+    if oper == '+':
+        return num1 + num2
+    elif oper == '-':
+        return num1 - num2
+    elif oper == '*':
+        return num1 * num2
+    elif oper == '%':
+        return num1 % num2
+
+#Main code (DO NOT CHANGE)
+
+num1 = get_random_between_1_10()
+oper = get_random_operation()
+num2 = get_random_between_1_10()
+result = calc_result(num1, oper, num2)
+
+print(f"{num1} {oper} {num2} = ?")
+user_result = int(input('whats the result? '))
+
+if result == user_result:
+    print('Correct!')
+else:
+    print(f"Wrong.. the answer was {result}")
